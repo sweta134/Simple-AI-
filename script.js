159e9ef60bbffd83a524bbcbdb6f95811aedadfc
Sweta134 <upadhyaysweta17@gmail.com>
2023-12-20 20:20:10 +0530

    Simple AI application

---
+++ script.js
@@ -0,0 +1,24 @@
+import {config} from 'dotenv';
+config();
+
+import OpenAI from 'openai';
+import readline from 'readline';
+
+const openai = new OpenAI({
+  apiKey: process.env.API_KEY 
+});
+
+const userInterface=readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+userInterface.prompt()
+userInterface.on("line", async input=>{
+    const chatCompletion = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [{"role": "user", "content": input}],
+      });
+      console.log(chatCompletion.choices[0].message.content);
+      userInterface.prompt()
+})
